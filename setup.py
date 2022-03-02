2957bdac52a39bf5ad9b01a5cf668b9c44560f0f
Andrea Maracani <andreamaracani@gmail.com>
2022-03-02 15:09:28 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
 
 VERSION = '0.0.1' 
 DESCRIPTION = 'Image datasets for Pytorch.'
-LONG_DESCRIPTION = 'Some classes that help to load images, as dataset, for Pytorch projects.'
 
 # Setting up
 setup(
@@ -13,6 +16,7 @@ setup(
         author_email="<andrea.maracani@iit.it>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
         packages=find_packages(),
         install_requires=[], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
